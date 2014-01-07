057e6e43dccc40a3917c3d875972560158dd368d
Martin Keckeis <martin.keckeis1@gmail.com>
2014-01-07 13:26:20 +0100

    Create Module.php

---
+++ Module.php
@@ -0,0 +1,40 @@
+<?php
+namespace ThaConfigalyzer;
+
+use Zend\ModuleManager\ModuleManager;
+use Zend\Mvc\MvcEvent;
+use Zend\EventManager\EventInterface;
+
+class Module
+{
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__
+                )
+            )
+        );
+    }
+
+    public function init(ModuleManager $manager)
+    {
+        $events = $manager->getEventManager();
+        
+        // Initialize logger collector once the profiler is initialized itself
+        $events->attach('profiler_init', function () use($manager)
+        {
+            $manager->getEvent()
+                ->getParam('ServiceManager')
+                ->get('configPerformance');
+        });
+    }
+}
+
